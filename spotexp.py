8225967556270df210472821633e141125896b98
Cyker Way <cykerway@gmail.com>
2014-10-19 23:46:30 -0400

    Update HTML parsing.

--- spotexp.py
+++ spotexp.py
@@ -2,24 +2,24 @@
 # -*- coding: utf-8 -*-
 
 # The MIT License (MIT)
-# 
+#
 # Copyright (c) 2012 Cyker Way
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy 
-# of this software and associated documentation files (the "Software"), to deal 
-# in the Software without restriction, including without limitation the rights 
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is furnished 
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is furnished
 # to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all 
+#
+# The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
-# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
-# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
-# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 '''Spotexp - Export playlists on Spotify.
@@ -37,13 +37,7 @@ Usage:
 
 Output format:
 
-[title1]
-[artist1]
-[album1]
-[title2]
-[artist2]
-[album2]
-......
+    [uri];[title];[time];[artist1,artist2...];[album]
 '''
 
 import httplib2
@@ -62,20 +56,38 @@ def download(listfile):
         items = f.readline().split()
 
     for item in items:
+
+        # url format: http://open.spotify.com/track/xxxxxxxxxxxxxxxxxxxxxx
         url = item.strip()
 
+        # uri format: xxxxxxxxxxxxxxxxxxxxxx
+        uri = url.split('/')[-1]
+
+        # Download and parse HTML page.
         r, c = h.request(url, 'GET')
+        root = etree.HTML(c)
 
-        parser = etree.HTMLParser(encoding='UTF-8')
-        root = etree.HTML(c, parser = parser)
-        title_node = root.xpath('//div[@id="metadata"]/h1[@id="title"]/a[@id="title"]')[0]
-        artist_node = root.xpath('//div[@id="metadata"]/div[@id="artist"]/p[@class="meta-info"]/a')[0]
-        album_node = root.xpath('//div[@id="metadata"]/div[@id="album"]/p[@class="meta-info"]/a')[0]
-        title, artist, album = title_node.text, artist_node.text, album_node.text
+        # Get album.
+        album_node = root.xpath('//div[@class="h-data"]/h1[@class="h-title"]')[0]
+        album = album_node.text
 
-        print title.encode('UTF-8')
-        print artist.encode('UTF-8')
-        print album.encode('UTF-8')
+        song_node = root.xpath('//div/table/tbody/tr[@data-uri="spotify:track:{}"]'.format(uri))[0]
+
+        # Get title.
+        title_node = song_node.xpath('td/div/strong[@class="tl-name-name"]')[0]
+        title = title_node.text
+
+        # Get artists.
+        artists_node = song_node.xpath('td/div/div[@class="tl-name-more text-muted"]')[0]
+        artists = re.sub(r'\s*,\s*', ',', artists_node.text.strip())
+
+        # Get time.
+        time_node = song_node.xpath('td[@class="tl-cell tl-time"]/span')[0]
+        time = time_node.text
+
+        # Print result.
+        result = ';'.join([uri, title, time, artists, album])
+        print result
 
 def usage():
     '''Echo usage and exit.'''
@@ -92,6 +104,4 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         usage()
 
-    listfile = sys.argv[1]
-
-    download(listfile)
+    download(sys.argv[1])
